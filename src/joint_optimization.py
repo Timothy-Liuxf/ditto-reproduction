9960935924e616166317835e74428ccbaa7fc357
Timothy Liu <liuxf19@163.com>
2023-12-19 22:46:49 +0800

    Add strategy

--- src/joint_optimization.py
+++ src/joint_optimization.py
@@ -2,8 +2,14 @@ from job import *
 from bottom_up_dop import bottom_up_dop
 from server import Server
 from typing import List
+from enum import Enum
 
-def joint_optimization(job: Job, servers: List[Server]) -> float:
+class Strategy(Enum):
+    DITTO = 0,
+    AVERAGE = 1,
+    RATIO = 2
+
+def joint_optimization(job: Job, servers: List[Server], strategy: Strategy) -> float:
     '''
     job: Job is the job to be scheduled
     return: float is the total execution time of the job
