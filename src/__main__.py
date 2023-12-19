22009047dbbb2025cfb1ac726377b57aca82840e
Timothy Liu <liuxf19@163.com>
2023-12-19 22:48:05 +0800

    Fix typos

--- src/__main__.py
+++ src/__main__.py
@@ -5,8 +5,10 @@ def main():
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
     print("Reproduction of 'Ditto'")
-    f = joint_optimization(Job([], [], 0), [])
-    print("Total execution time: {}".format(f))
+    f0 = joint_optimization(Job([], [], 0), [], Strategy.DITTO)
+    f1 = joint_optimization(Job([], [], 0), [], Strategy.AVERAGE)
+    f2 = joint_optimization(Job([], [], 0), [], Strategy.RATIO)
+    print("Total execution time: {}".format(f0, f1, f2))
 
 if __name__ == "__main__":
     main()
